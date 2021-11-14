993ef8396c81a96498efb19809f98bf7404336a1
zyxd <zyxd@syncraft.ru>
2021-11-14 17:00:17 +0400

    Fix bug with multiple components on the page

--- src/Rating.svelte
+++ src/Rating.svelte
@@ -1,22 +1,22 @@
 <svg width={width_total} height={height} viewBox={`0 0 ${width_total} ${height}`}>
   <defs>
-    <mask id="perc">
+		<mask id={`perc-${random}`}>
       <rect x="0" y="0" width="100%" height="100%" fill="#fff" />
       <rect x={`${$value_tweened * 100}%`} y="0" width="100%" height="100%" fill="#222" />
     </mask>
 
-    <symbol viewBox={`0 0 ${width} ${height}`} id="star">
+    <symbol viewBox={`0 0 ${width} ${height}`} id={`star-${random}`}>
       <path d="M31.547 12a.848.848 0 00-.677-.577l-9.427-1.376-4.224-8.532a.847.847 0 00-1.516 0l-4.218 8.534-9.427 1.355a.847.847 0 00-.467 1.467l6.823 6.664-1.612 9.375a.847.847 0 001.23.893l8.428-4.434 8.432 4.432a.847.847 0 001.229-.894l-1.615-9.373 6.822-6.665a.845.845 0 00.214-.869z" />
     </symbol>
 		
-    <symbol viewBox={`0 0 ${width_total} ${height}`} id="stars">
+    <symbol viewBox={`0 0 ${width_total} ${height}`} id={`stars-${random}`}>
 			{#each {length: max} as _, index}
-				<use xlink:href="#star" x={get_offset(width, max, index)} y="0"></use>
+				<use xlink:href={`#star-${random}`} x={get_offset(width, max, index)} y="0"></use>
 			{/each}
     </symbol>
   </defs>
 
-  <use xlink:href="#stars" fill={color} stroke="black" mask="url(#perc)"></use>
+  <use xlink:href={`#stars-${random}`} fill={color} stroke="black" mask={`url(#perc-${random})`}></use>
 </svg>
 
 <script>
@@ -27,6 +27,8 @@
 	export let value = 0
 	export let color = 'red'
 	export let duration = 1000
+
+	const random = Math.floor(Math.random() * 1000000);
 	
 	const value_tweened = tweened(0, {
 		duration,
